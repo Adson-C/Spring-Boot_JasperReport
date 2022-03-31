030a0813baea94d96aa26d7004440e02dc735281
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-03-31 00:19:26 -0300

    Abrindo o Relatório com Parametros via URL

--- src/main/java/com/mballem/curso/jasper/spring/controller/JasperController.java
+++ src/main/java/com/mballem/curso/jasper/spring/controller/JasperController.java
@@ -1,20 +1,31 @@
 package com.mballem.curso.jasper.spring.controller;
 
+import com.mballem.curso.jasper.spring.repository.EnderecoRepository;
+import com.mballem.curso.jasper.spring.repository.NivelRepository;
 import com.mballem.curso.jasper.spring.service.JasperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class JasperController {
 
     @Autowired
     private JasperService service;
+
+    @Autowired
+    private NivelRepository nivelRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
     @GetMapping("/reports")
     public String abrir() {
@@ -34,4 +45,28 @@ public class JasperController {
         }
         response.getOutputStream().write(bytes);
     }
+
+    @GetMapping("/relatorio/pdf/jr9/{code}")
+    public void exibirRelatorio09(@PathVariable("code") String code,
+                                  @RequestParam(name="nivel", required = false) String nivel,
+                                  @RequestParam(name="uf", required = false) String uf,
+                                  HttpServletResponse response) throws IOException {
+        service.addParams("NIVEL_DESC", nivel.isEmpty() ? null : nivel);
+        service.addParams("UF", uf.isEmpty() ? null : uf);
+
+        byte[] bytes = service.exportarPDF(code);
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        response.setHeader("Content-disposition", "inline; filename=relatorio-" + code + ".pdf" );
+        response.getOutputStream().write(bytes);
+    }
+
+    @ModelAttribute("niveis")
+    public List<String> getNiveis() {
+        return nivelRepository.findNiveis();
+    }
+    @ModelAttribute("ufs")
+    public List<String> getUfs() {
+        return enderecoRepository.findUfs();
+    }
 }
+
